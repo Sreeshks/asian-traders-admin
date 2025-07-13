fc2713a63edfaeedddde785176ee0ebe78faaaff
Sreeshks <sreeshksureshh@gmail.com>
2025-07-13 10:20:24 +0530

    updated

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,7 +7,9 @@ export default function Layout({ children, onDashboardClick, onCategoryClick, on
     <div className="min-h-screen bg-gradient-to-br from-gray-100 to-blue-100 flex font-sans">
       <Sidebar onDashboardClick={onDashboardClick} onCategoryClick={onCategoryClick} onProductClick={onProductClick} />
       <div className="flex-1 flex flex-col">
-        <Header />
+        <div className="hidden md:block">
+          <Header />
+        </div>
         <div className="pt-16 md:pt-0">
           {children}
         </div>
