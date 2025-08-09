e2cb9556ca87355007118f74d18013c6c95bb24a
SREESH K SURESH <138683711+Sreeshks@users.noreply.github.com>
2025-08-09 09:56:19 +0530

    new server changed

--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
 // src/common.js
  
 // export const API_BASE_URL = 'https://asian-traders-backend.onrender.com'; 
-export const API_BASE_URL = 'https://asiantradersbackend-production-a429.up.railway.app'; //Live
+export const API_BASE_URL = 'https://asiantradersbackend-production-7be7.up.railway.app'; //Live
